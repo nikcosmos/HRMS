32d46e6fdded8c163e2d8028d5e8556ae0249dd3
Otrya <babenko.olia@gmail.com>
2023-12-27 18:58:12 +0200

    feature: add header

--- components/Header/index.vue
+++ components/Header/index.vue
@@ -1,3 +1,11 @@
+
 <template>
-   <div class="container max-w-7xl">Header</div>
+   <div class="container max-w-7xl h-32 flex items-center justify-around">
+      <NuxtLink to="/" class="h-full w-auto"><img src="/images/hrms-green.svg" alt="logo" class="h-full object-contain"/></NuxtLink>
+      <HeaderNavigation/>
+      <button class="focus:outline-none focus-visible:outline-0 flex font-medium gap-x-2 px-3 py-2 text-sm">
+         <span class="">Log in</span>
+         <span class=" flex-shrink-0 h-5 w-5"><img src="/images/arrow-right.svg" alt="arrow-right" class="h-full object-contain"></span>
+      </button>
+   </div>
 </template>
