07fb8d66dbe9ca758237747309054256c950545f
Nik Horbachov <nikcosmos@gmail.com>
2024-01-08 13:03:19 +0200

    refactor: style

--- components/Header/Language.vue
+++ components/Header/Language.vue
@@ -3,8 +3,12 @@ const switchLocalePath = useSwitchLocalePath();
 </script>
 
 <template>
-   <div>
-      <NuxtLink :to="switchLocalePath('ua')" class="language__btn"> Ua </NuxtLink>
-      <NuxtLink :to="switchLocalePath('en')" class="language__btn"> En </NuxtLink>
+   <div class="flex gap-2">
+      <NuxtLink :to="switchLocalePath('ua')" class="group">
+         <span class="font-bold group-[.router-link-active]:text-red-500">Ua</span>
+      </NuxtLink>
+      <NuxtLink :to="switchLocalePath('en')" class="group">
+         <span class="font-bold group-[.router-link-active]:text-red-500">En</span>
+      </NuxtLink>
    </div>
 </template>
