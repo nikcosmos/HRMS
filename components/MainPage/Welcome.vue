c2ec8f568a45b8695fb039ff012ec24374ccfb88
Nik Horbachov <nikcosmos@gmail.com>
2024-01-08 12:03:03 +0200

    chore: sorting

--- components/MainPage/Welcome.vue
+++ components/MainPage/Welcome.vue
@@ -1,22 +1,22 @@
 <template>
-   <div class="container text-center py-36">
-      <div class="flex justify-center mb-3">
-         <span class="px-2 py-1 ring-2 rounded-full text-12 font-bold max-w-fit">
+   <div class="container py-36 text-center">
+      <div class="mb-3 flex justify-center">
+         <span class="max-w-fit rounded-full px-2 py-1 text-12 font-bold ring-2">
             BETA-версія
          </span>
       </div>
-      <h1 class="text-24 575:text-36 768:text-52 font-semibold mb-5 max-w-3xl mx-auto">
+      <h1 class="mx-auto mb-5 max-w-3xl text-24 font-semibold 575:text-36 768:text-52">
          Керуй людськими ресурсами з комфортом
       </h1>
-      <p class="italic text-14">Зроблено людьми для людей</p>
-      <div class="mt-10 flex flex-wrap gap-x-6 gap-y-3 justify-center">
+      <p class="text-14 italic">Зроблено людьми для людей</p>
+      <div class="mt-10 flex flex-wrap justify-center gap-x-6 gap-y-3">
          <button
             type="button"
-            class="group text-14 flex items-center gap-2 py-3 px-4 rounded-full bg-blue-300 dark:text-stone-800 font-bold transition-colors hover:bg-blue-200"
+            class="group flex items-center gap-2 rounded-full bg-blue-300 px-4 py-3 text-14 font-bold transition-colors hover:bg-blue-200 dark:text-stone-800"
          >
             Спробувати
             <IconArrowRight
-               class="w-5 h-5 flex font-bold group-hover:translate-x-0.5 transition-transform"
+               class="flex h-5 w-5 font-bold transition-transform group-hover:translate-x-0.5"
             />
          </button>
       </div>
