884b23336af1eaea91ceff6e3e4c6bcfd98aa114
Otrya <babenko.olia@gmail.com>
2024-01-08 14:15:44 +0200

    burger

---
+++ components/Header/Burger.vue
@@ -0,0 +1,16 @@
+<script setup>
+    // Burger
+    const store = useMainStore();
+    const { openBurger, closeBurger } = store;
+    const { burger } = storeToRefs(store);
+    console.log(burger)
+</script>
+
+<template>
+    <div @click="burger ? closeBurger : openBurger">
+        <div  class="h-5 w-5">
+            <img :src="burger ? '/images/x-mark.svg' : '/images/burger-menu.svg'" alt="burger" class="object-contain h-full"/>
+        </div>
+        <p>{{ burger }}</p>
+    </div>
+</template>
--- components/Header/index.vue
+++ components/Header/index.vue
@@ -3,17 +3,29 @@
 const store = useMainStore();
 const { setTheme } = store;
 const { theme } = storeToRefs(store);
+//icons
+import {sunIcon} from '/images/sun-icon.svg';
+const sun = ref(sunIcon)
 </script>
 
 <template>
    <div class="container max-w-7xl h-32 flex items-center justify-around">
-      <NuxtLink to="/" class="h-full w-auto"><img src="/images/hrms-green.svg" alt="logo" class="h-full object-contain" />
+      <NuxtLink to="/" class="h-full w-auto">
+         <img src="/images/hrms-green.svg" alt="logo" class="h-full object-contain" />
       </NuxtLink>
       <HeaderNavigation />
-      <input :checked="theme" @change="setTheme" type="checkbox" name="theme" />
+      <label>
+
+         <input :checked="theme" @change="setTheme" type="checkbox" name="theme" />
+      </label>
+
       <button class="focus:outline-none focus-visible:outline-0 flex font-medium gap-x-2 px-3 py-2 text-sm">
          <span class="">Log in</span>
-         <span class="flex-shrink-0 h-5 w-5 object-contain"><img src="/images/arrow-right.svg" alt="arrow-right"></span>
+         <span class="h-5 w-5"><img src="/images/arrow-right.svg" alt="arrow-right"></span>
       </button>
+      <div>
+         <HeaderBurger/>
+      </div>
    </div>
+   
 </template>
