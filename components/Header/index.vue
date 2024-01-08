a8cd973fd64a1088c84c0442e33e678d57531075
Nik Horbachov <nikcosmos@gmail.com>
2024-01-08 12:47:39 +0200

    feature: add switcher

--- components/Header/index.vue
+++ components/Header/index.vue
@@ -8,5 +8,6 @@ const { theme } = storeToRefs(store);
 <template>
    <div class="container max-w-7xl">
       <input :checked="theme" @change="setTheme" type="checkbox" name="theme" />
+      <HeaderLanguage />
    </div>
 </template>
