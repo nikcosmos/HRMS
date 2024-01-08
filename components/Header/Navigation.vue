888f98066522d6c4bd301b274ce66813a64cb636
Nik Horbachov <nikcosmos@gmail.com>
2024-01-08 13:03:57 +0200

    chore: add new component

---
+++ components/Header/Navigation.vue
@@ -0,0 +1,11 @@
+
+<template>
+    <div>
+        <nav class="flex ring-1 ring-gray-800 px-3 rounded-full">
+            <NuxtLink to="#" class="font-medium py-2 px-4">Features</NuxtLink>
+            <NuxtLink to="#" class="font-medium py-2 px-4">Pricing</NuxtLink>
+            <NuxtLink to="#" class="font-medium py-2 px-4">Testmonials</NuxtLink>
+            <NuxtLink to="#" class="font-medium py-2 px-4">FAQ</NuxtLink>
+        </nav>
+    </div>
+</template>
--- components/Header/index.vue
+++ components/Header/index.vue
@@ -6,8 +6,21 @@ const { theme } = storeToRefs(store);
 </script>
 
 <template>
-   <div class="container max-w-7xl">
+   <div class="container flex h-32 max-w-7xl items-center justify-around">
+      <NuxtLink to="/" class="h-full w-auto"
+         ><img src="/images/hrms-green.svg" alt="logo" class="h-full object-contain" />
+      </NuxtLink>
+      <HeaderNavigation />
+      <button
+         class="text-sm flex gap-x-2 px-3 py-2 font-medium focus:outline-none focus-visible:outline-0"
+      >
+         <span class="">Log in</span>
+         <span class="h-5 w-5 flex-shrink-0 object-contain"
+            ><img src="/images/arrow-right.svg" alt="arrow-right"
+         /></span>
+      </button>
       <input :checked="theme" @change="setTheme" type="checkbox" name="theme" />
+
       <HeaderLanguage />
    </div>
 </template>
