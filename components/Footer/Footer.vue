42db65e295018eca4baf614887e634748878c487
Bookkeeper33 <72704006+Bookkeeper33@users.noreply.github.com>
2024-01-09 17:11:12 +0200

    fix: footer correction

--- components/Footer/Footer.vue
+++ components/Footer/Footer.vue
@@ -2,8 +2,8 @@
 
 <template>
    <div class="max-w-full">
-      <div class="border-t border-gray-300 dark:border-gray-700 p-3">
-         <div class="container flex flex-col items-center gap-10 p-7">
+      <div class="border-t border-gray-300 dark:border-gray-700">
+         <div class="container flex flex-col items-center gap-10 px-3 py-7 768:px-7">
             <FooterForm />
             <FooterNav />
          </div>
--- components/Footer/FooterCopy.vue
+++ components/Footer/FooterCopy.vue
@@ -4,14 +4,14 @@ const year = ref(new Date().getFullYear());
 
 <template>
    <div
-      class="mx-auto px-4 575:px-6 1200:px-8 max-w-7xl py-8 1200:py-4 flex flex-col-reverse 575:flex-row items-center justify-between gap-x-3 gap-y-5"
+      class="mx-auto flex max-w-7xl flex-col-reverse items-center justify-between gap-x-3 gap-y-5 px-6 py-8 575:flex-row"
    >
-      <div class="flex items-center justify-center 575:justify-start flex-1 gap-x-1.5">
-         <p class="text-center text-black dark:text-gray-400 text-16">
+      <div class="flex items-center justify-center gap-x-1.5 575:justify-start">
+         <p class="text-center text-14 text-black dark:text-gray-400">
             Copyright © {{ year }}. All rights reserved.
          </p>
       </div>
-      <div class="575:flex-1 flex items-center justify-center 575:justify-end gap-x-1.5">
+      <div class="flex items-center justify-center gap-x-1.5 575:justify-end">
          <UIThemeSwitch />
       </div>
    </div>
